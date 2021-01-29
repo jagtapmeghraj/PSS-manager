204cf91b7fe35cc3712885950a286be970837f04
meghraj <jagtapmeghraj05@gmail.com>
2021-01-30 00:56:01 +0530

    project setup

--- PSS-manager/src/main/java/io/myproject/PSSmanager/PssManagerApplication.java
+++ PSS-manager/src/main/java/io/myproject/PSSmanager/PssManagerApplication.java
@@ -1,4 +1,4 @@
-package com.example.PSSmanager;
+package io.myproject.PSSmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PssManagerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PssManagerApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
--- PSS-manager/src/test/java/io/myproject/PSSmanager/PssManagerApplicationTests.java
+++ PSS-manager/src/test/java/io/myproject/PSSmanager/PssManagerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.PSSmanager;
+package io.myproject.PSSmanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
