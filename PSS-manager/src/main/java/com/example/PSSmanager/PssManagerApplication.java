b55c4c5e57b06a32e5fde805a2dedc74c4b9f297
meghraj <jagtapmeghraj05@gmail.com>
2021-01-30 00:16:45 +0530

    first commit

---
+++ PSS-manager/src/main/java/com/example/PSSmanager/PssManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.PSSmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PssManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PssManagerApplication.class, args);
+	}
+
+}
---
+++ PSS-manager/src/test/java/com/example/PSSmanager/PssManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PSSmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PssManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
